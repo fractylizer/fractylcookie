6f8019f2b3822415fba39feed6ca41c89e94ce84
Laura Trevisan <129926917+fractylizer@users.noreply.github.com>
2024-06-03 03:41:07 +1200

    Add mod functions and achievements

---
+++ fractylcookie.js
@@ -0,0 +1,57 @@
+Game.Win('Third-party');
+
+Game.registerMod('fractylCookie',{
+  init:function(){
+    Game.registerHook('check', this.check);
+    if(Game.ready) {this.create()}
+    else {Game.registerHook('create', this.create)};
+  },
+  save:function(){
+    let objToSave = {
+      achievements:"",
+    };
+    for(let i of this.achievements) {objToSave.achievements+=i.won};
+    return JSON.stringify(objToSave);
+  },
+  load:function(str){
+    var data = JSON.parse(str);
+    for(let i in data.achievements) {this.achievements[i].won = Number(data.achievements[i])}
+  },
+  addAchievement:function(name,desc,icon,achorder,pool) {
+    this.achievements.push(new Game.Achievement(name,desc,icon))
+    if (pool == "shadow") {
+      Game.last.pool = 'shadow';
+    }
+    Game.last.order = achorder;
+  },
+  achievements: [],
+  create:function() {
+    Game.Loader.Replace('wrinkler.png',`https://fractylizer.github.io/fractylcookie/img/wrinkler.png`);
+    Game.Loader.Replace('perfectCookie.png',`https://fractylizer.github.io/fractylcookie/img/perfectCookie.png`);
+    Game.Objects['You'].sellFunction=function(){Game.Win('Self-sacrifice')}
+    this.createAchievements()
+  },
+  check:function() {
+    Game.mods['fractylCookie'].checkAchievements();
+  },
+  createAchievements:function() {
+    this.addAchievement("Pretty pink priorities", "Shape your clones as <b>the default, with pink skin.</b>",[1,0,this.icons],32600,'shadow');
+    this.addAchievement("Regular person complex", "Name yourself <b>Fractyl</b>.<div class=\"warning\">Note: this doesn't have any penalties.</div><q>Out of everyone you could have named yourself after?</q>",[0,0,this.icons],30200,'shadow');
+    this.addAchievement("Self-sacrifice", "Sell a You.<q>We're all so sad to see you go.</q>",[35,0],2600,'normal');
+    this.addAchievement("Really?", "Use the <b>Extra Content Mod</b>.<q>I thought you had a life.<br>Seems I was mistaken.</q>",[2,0,this.icons],69421,'shadow');
+    LocalizeUpgradesAndAchievs();
+  },
+  checkAchievements:function() {
+    if (Game.YouCustomizer.currentGenes[0]==0
+      && (Game.YouCustomizer.currentGenes[2]==12)
+      && (Game.YouCustomizer.currentGenes[3]==0)
+      && (Game.YouCustomizer.currentGenes[4]==0)
+      && (Game.YouCustomizer.currentGenes[5]==0)
+      && (Game.YouCustomizer.currentGenes[5]==0)
+    ) {Game.Win('Pretty pink priorities')};
+    if (Game.bakeryName.toLowerCase() =='fractyl') {Game.Win('Regular person complex')};
+    if (Game.mods['extraContent'] !== undefined) {Game.Win('Really?')}
+  },
+  url:"https://fractylizer.github.io/fractylcookie/",
+  icons:`https://fractylizer.github.io/fractylcookie/img/icons.png`
+});
