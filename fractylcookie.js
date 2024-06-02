1947fbeb1a379eb6f60a2434aae03612d2b51626
Laura Trevisan <129926917+fractylizer@users.noreply.github.com>
2024-06-03 04:03:05 +1200

    Add WTCAJN

--- fractylcookie.js
+++ fractylcookie.js
@@ -3,6 +3,7 @@ Game.Win('Third-party');
 Game.registerMod('fractylCookie',{
   init:function(){
     Game.registerHook('check', this.check);
+    Game.registerHook('reset', function(hardreset) {this.reset(hardreset)});
     if(Game.ready) {this.create()}
     else {Game.registerHook('create', this.create)};
   },
@@ -34,11 +35,17 @@ Game.registerMod('fractylCookie',{
   check:function() {
     Game.mods['fractylCookie'].checkAchievements();
   },
+  reset:function(hardreset) {
+    if (!hardreset) {
+      if (Math.round(Game.cookies)==69000000000000) Game.Win('When the cookies ascend just nice');
+    }
+  },
   createAchievements:function() {
     this.addAchievement("Pretty pink priorities", "Shape your clones as <b>the default, with pink skin.</b>",[1,0,this.icons],32600,'shadow');
-    this.addAchievement("Regular person complex", "Name yourself <b>Fractyl</b>.<div class=\"warning\">Note: this doesn't have any penalties.</div><q>Out of everyone you could have named yourself after?</q>",[0,0,this.icons],30200,'shadow');
+    this.addAchievement("Regular person complex", "Name yourself <b>Fractyl</b>.<div class=\"warning\">Note: this doesn't have any penalties.</div><q>Out of everyone you could have named yourself after?</q>",[0,0,this.icons],30200.160,'shadow');
     this.addAchievement("Self-sacrifice", "Sell a You.<q>We're all so sad to see you go.</q>",[35,0],2600,'normal');
     this.addAchievement("Really?", "Use the <b>Extra Content Mod</b>.<q>I thought you had a life.<br>Seems I was mistaken.</q>",[2,0,this.icons],69421,'shadow');
+    this.addAchievement("When the cookies ascend just nice", loc("Ascend with exactly <b>%1</b>.",loc("%1 cookie",LBeautify(6.9e13))),[25,7],30250.398,'shadow');
     LocalizeUpgradesAndAchievs();
   },
   checkAchievements:function() {
