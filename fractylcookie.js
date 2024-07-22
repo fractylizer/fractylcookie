8aeda02dfddfa6413098caf13ef8b2f4a0a9caf8
Laura Trevisan <129926917+fractylizer@users.noreply.github.com>
2024-07-22 18:18:03 +1200

    Add inverted cookies

--- fractylcookie.js
+++ fractylcookie.js
@@ -58,6 +58,7 @@ Game.registerMod('fractylCookie',{
 		this.addCookieUpgrade({name:'Fractyl cookies',desc:'A mostly plain cookie, with a white chocolate logo. A delicious reminder to give Fractyl all your money.',icon:[0,1,this.icons],power:5,price:9999999999999999*5},10020.2575);
 		this.addCookieUpgrade({name:'Red velvet cookies',desc:'Fancy! The presence of white chocolate chips is a given.',icon:[1,1,this.icons],power:2,price:9999999999*5},10003);
     this.addCookieUpgrade({name:'Compact discs',desc:'Despite what you might assume, these are often not interchangable with cookies.',icon:[2,1,this.icons],require:'Box of not cookies',power:5,price:Math.pow(10,48)},10061)
+    this.addCookieUpgrade({name:'Inverted cookies',desc:'The result of extensive photo manipulation. A magical sight.',icon:[4,0,this.icons],require:'Box of maybe cookies',power:5,price:Math.pow(10,49)},10051)
     LocalizeUpgradesAndAchievs();
   },
   createAchievements:function() {
